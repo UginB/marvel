879f21ae1f6e15b6006e3f804fd93ecedb58f653
Evgeniy Bratenkin <ugin-bro@ya.ru>
2021-12-26 19:17:26 +0300

    add helmet

--- src/components/Pages/SingleComicPage.js
+++ src/components/Pages/SingleComicPage.js
@@ -1,3 +1,4 @@
+import { Helmet } from 'react-helmet';
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
@@ -47,6 +48,13 @@ const View = ({comic}) => {
     const {title, description, thumbnail, pageCount, language, price} = comic;
     return (
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
